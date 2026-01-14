65f4cc00d46d33ffa9725bf955d92435a8fb9966
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-14 20:11:31 +0530

    Sidebar

---
+++ Instagram/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <div className='d-flex vh-100'>
+      <div className='w-20'><Sidebar/></div>
+      <div className='w-50 bg-secondary'>feed</div>
+      <div className='w-30'>suggestions</div>
+    </div>
+  )
+}
+
+export default App
---
+++ Instagram/src/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="m-3">
+      <div className="d-flex flex-column gap-3">
+        <img className="insta-text" src="src\assets\insta-text.png" alt="" />
+        <div><i class="bi bi-house-door"></i>Home</div>
+        <div><i class="bi bi-search"></i>Search</div>
+        <div><i class="bi bi-compass"></i>Explore</div>
+        <div><i class="bi bi-film"></i>Reels</div>
+        <div><i class="bi bi-chat-dots"></i>Messages</div>
+        <div><i class="bi bi-heart-fill"></i>Notifications</div>
+        <div><i class="bi bi-plus-square"></i>Create</div>
+        <div><i class="bi bi-person-circle"></i>Profile</div>
+      </div>
+      <div className="position-fixed bottom-0 d-flex flex-column gap-3 mb-3">
+        <div><i class="bi bi-threads"></i>Threads</div>
+        <div><i class="bi bi-list"></i>More</div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
