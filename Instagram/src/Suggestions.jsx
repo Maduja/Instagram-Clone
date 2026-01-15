43233a0ff91f7aec61e670f74f17d6d37b5f6ce4
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-15 13:09:13 +0530

    suggestions

--- Instagram/src/Suggestions.jsx
+++ Instagram/src/Suggestions.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Suggestions() {
+  const [profile, setProfile] = useState(null);
+  const [suggestions, setSuggetions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/profile")
+      .then((data) => data.json())
+      .then((data) => setProfile(data))
+      .catch((err) => console.log(err));
+
+    fetch("http://localhost:3000/suggestions")
+      .then((data) => data.json())
+      .then((data) => setSuggetions(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-        Suggestions
+      <div className="suggestions w-75 m-4">
+        {profile ? (
+          <div className="d-flex">
+            <img
+              className="dp rounded-circle"
+              src={profile.profilePic}
+              alt="profile pic"
+            />
+            <h5>{profile.username}</h5>
+            <small className="ms-auto text-primary">Switch</small>
+          </div>
+        ) : (
+          <p>Loading</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Suggestions
+export default Suggestions;
