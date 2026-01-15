85138af001b50b9b4c8f730b545d84304644d26d
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-16 01:07:48 +0530

    sidebar hover

--- Instagram/src/Sidebar.jsx
+++ Instagram/src/Sidebar.jsx
@@ -10,36 +10,36 @@ function Sidebar() {
     <div className="m-3 position-fixed">
       <div className="d-flex flex-column gap-3">
         <img className="insta-text" src="src\assets\insta-text.png" alt="" />
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-house-door"></i>Home
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-search"></i>Search
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-compass"></i>Explore
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-film"></i>Reels
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-chat-dots"></i>Messages
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-heart-fill"></i>Notifications
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-plus-square"></i>Create
         </div>
-        <div onClick={()=>{navigate('/profile')}}>
+        <div onClick={()=>{navigate('/profile')}} className="sidebar-item">
           <i className="bi bi-person-circle"></i>Profile
         </div>
       </div>
       <div className="position-fixed bottom-0 d-flex flex-column gap-3 mb-3">
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-threads"></i>Threads
         </div>
-        <div>
+        <div className="sidebar-item">
           <i className="bi bi-list"></i>More
         </div>
       </div>
